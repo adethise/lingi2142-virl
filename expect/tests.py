8bad960959df580981c0c52b76eb904038a6dd32
Arnaud Dethise <arnauddethise@gmail.com>
2016-03-20 17:32:31 +0100

    correct ping testing

--- expect/tests.py
+++ expect/tests.py
@@ -4,58 +4,12 @@ import pexpect
 import sys
 
 
-if len(sys.argv) != 3:
-    print('USAGE:    %s IP PORT_RANGE' % sys.argv[0])
-    print('EXAMPLE:  %s %s %s' % (sys.argv[0], '172.156.58.2', '17000-17010'))
-else:
-    __main__(sys.argv)
-
-
-
-def test_ping(telnet_info, params):
-    """
-    Test ping to each router.
-    @param router = tuple (ip, port) for telnet connexion
-    @param params = list of ips to ping
-    """
-
-    print('Testing ping from connexion telnet://%s:%s' % telnet_info)
-
-    child = pexpect.spawn('telnet %s %s' % telnet_info, timeout=1)
-
-    try:
-        index = child.expect(['uefquqebqoub'])
-
-        print('Connexion established...')
-
-        for ip in params:
-            print('\tPinging ip %s... ' % ip, end='')
-
-            child.sendline('ping %s' % ip)
-            index = child.expect(['ip success'])
-
-
-            print('Success')
-
-        print('Closing connexion.\n')
-
-        #child.sendline('exit')
-        #child.expect(pexpect.EOF)
-
-        return True
-
-    except (pexpect.EOF, pexpect.TIMEOUT):
-        print('Test failed')
-        return False
-        
-
 def __main__(argv):
     telnet_ip = argv[1]
-    (telnet_port_start, telnet_port_end) = map(int, argv[2].split('-'))
-    telnet_ports = map(str, range(telnet_port_start, telnet_port_end+1))
+    telnet_ports = argv[2].split(',')
 
     connexions = [(telnet_ip, telnet_port) for telnet_port in telnet_ports]
-    routers = ['192.168.0.%d' % d for d in range(10)]
+    routers = ['192.168.0.%d' % d for d in range(1, 10)]
 
     ##############
     # Test: ping #
@@ -80,3 +34,64 @@ def __main__(argv):
     #############
     # Test: ... #
     #############
+
+
+def test_ping(telnet_info, params):
+    """
+    Test ping to each router.
+    @param router = tuple (ip, port) for telnet connexion
+    @param params = list of ips to ping
+    """
+
+    print('Testing ping from connexion telnet://%s:%s' % telnet_info)
+
+    (telnet_ip, telnet_port) = telnet_info
+    child = pexpect.spawn('telnet %s %s' % telnet_info)
+
+    error = 0
+
+    try:
+        child.expect([r'Connected to %s' % telnet_ip])
+        child.expect([r'Escape character is'])
+        child.expect([r'\r\n'])
+        child.sendline('')
+
+        print('Connexion established...')
+
+        for ip in params:
+            child.expect([r'[EC(Ext)]\d>']) # Waiting for prompt
+
+            print('\tPinging ip %s... ' % ip, end='')
+
+            child.sendline('ping %s' % ip)
+            child.expect([r'Success rate is '])
+            child.expect([r' '])
+            rate = int(child.before) # Get the success percentage
+
+            if rate < 50:
+                error += 1
+                print('Failure')
+            else:
+                print('Success')
+
+        print('Closing connexion.\n')
+
+        del child
+        #child.sendline('exit')
+        #child.expect(pexpect.EOF)
+
+        return error == 0
+
+    except (pexpect.EOF, pexpect.TIMEOUT):
+        print('Test failed')
+        return False
+        
+###############################################################################
+
+
+if len(sys.argv) != 3:
+    print('USAGE:    %s IP PORT[,PORT...]' % sys.argv[0])
+    print('EXAMPLE:  %s %s %s' % (sys.argv[0], '172.156.58.2', '17000,17002,17012'))
+else:
+    __main__(sys.argv)
+
